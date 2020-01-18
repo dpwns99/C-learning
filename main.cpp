604fcea29a200289ad80b6efa41137ef5d6c9d5f
Triangleyj <dpwns99@gmail.com>
2020-01-18 22:13:46 +0900

    iosteram chapter finished.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
+#include <windows.h> // initial korean setting
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
 int main() {
-    SetConsoleOutputCP(65001);
+    SetConsoleOutputCP(65001); // initial korean setting
 
-    cout << "안녕하세요" << endl;
+    int age;
+
+    cout << "나이를 입력해 주세요 : ";
+    cin >> age;
+
+    cout << "여러분의 나이는 " << age << "살 입니다." << endl;
     return 0;
 }
