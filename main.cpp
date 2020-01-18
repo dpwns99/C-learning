0927a01cce6d30119ea5ddfef33fa50e1ea9734e
Triangleyj <dpwns99@gmail.com>
2020-01-18 22:28:24 +0900

    modified main cpp file.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-#include <windows.h> // initial korean setting
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    SetConsoleOutputCP(65001); // initial korean setting
+    std::cout << "Hello world" << std::endl;
 
-    int age;
-
-    cout << "나이를 입력해 주세요 : ";
-    cin >> age;
-
-    cout << "여러분의 나이는 " << age << "살 입니다." << endl;
     return 0;
 }
